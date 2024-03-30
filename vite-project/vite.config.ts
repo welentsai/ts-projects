b23eacb0009d2a1d6d4ec3133267ec86e0d5a2da
welentsai <welentsai@gmail.com>
2024-03-30 22:29:22 +0800

    vitest init

--- vite-project/vite.config.ts
+++ vite-project/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { configDefaults } from "vitest/config";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    // setupFiles: ["./testConfig.ts"],
+    coverage: {
+      provider: "v8",
+      // exclude: [...configDefaults.coverage.exclude, "*.config.js"],
+      all: true,
+    },
+  },
   plugins: [react()],
+  build: {
+    target: "esnext",
+  },
 })
